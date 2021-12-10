8bf8602e9b0814ae6c98fffb3d0b35d8c05af483
Ben Luo <4107322+bl1428@users.noreply.github.com>
2021-12-10 04:29:19 -0600

    add script for generating benchmarking run configs

---
+++ src/generate_configs.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+
+source("generate_data.R")
+
+# generate collection of run configurations for benchmarking
+main <- function() {
+    args <- commandArgs(trailingOnly=TRUE)
+    if(length(args) < 2) {
+      stop("Usage: generate_configs.R <base dir> <config file name>")
+    }
+
+    base.dir <- as.numeric(args[1])
+    config.file <- args[2]
+
+    # generate data
+    create.configurations(base.dir, config.file)
+}
+
+main()
